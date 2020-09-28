70c7bc7f974396c77e8b6bd30195deeede20a3e0
Adam Bridge <3991122+adambridge@users.noreply.github.com>
2020-09-28 21:49:05 +0100

    New smol version

--- install.sh
+++ install.sh
@@ -4,27 +4,36 @@
 sudo apt-get install dnsutils awscli jq
 
 # Create ~/bin and ~/.ip53 if they don't exist
-file -d ~/bin || mkdir ~/bin
-file -d ~/.ip53 || mkdir ~/.ip53
+[ -d ~/bin ] || mkdir ~/bin
+[ -d ~/.ip53 ] || mkdir ~/.ip53
  
 # Get directory that the install.sh and ip53.sh are in
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Create link in ~/bin pointing to ip53.sh 
 LNK=$(echo ~/bin/ip53)
-file -d $LNK || ln -s $SCRIPTDIR/ip53.sh $LNK
+[ -e $LNK ] || rm $LNK
+ln -s $SCRIPTDIR/ip53.sh $LNK
+
+# Copy template.json to ~/.ip53
+cp $SCRIPTDIR/json.tmpl ~/.ip53/json.tmpl
 
 echo Enter Route 53 hosted zone ID \(e.g. H6DWNY8UJ3Q1\):
-read ZONE
+read ZONE_ID
 echo Enter Route 53 record name \(e.g. my.domain.com\):
-read RECORD
+read RECORD_NAME
+
+cat << EOF > ~/.ip53/ip53.config
+ZONE_ID=$ZONE_ID
+RECORD_NAME=$RECORD_NAME
+EOF
 
 # Install crontab
 TMPFILE=$(mktemp /tmp/temporary-file.XXXXXXXX)
 crontab -l | grep -v "#IP53-AUTO-INSTALL$" > $TMPFILE
 
 cat <<EOF >> $TMPFILE
-*/5 * * * * bash $LNK --record=$RECORD --zone=$ZONE | sed "s/^/\$(date): /" >> ~/.ip53/ip53.log 2>&1 #IP53-AUTO-INSTALL
+*/5 * * * * bash $LNK | sed "s/^/\$(date): /" >> ~/.ip53/ip53.log 2>&1 #IP53-AUTO-INSTALL
 EOF
 
 crontab $TMPFILE
